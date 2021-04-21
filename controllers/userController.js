bbe1ce63b59bd05d2c523c19b70d42fc5d653438
GeorgeChal <daxter132@gmail.com>
2021-04-21 22:30:18 +0300

    model user before validate

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const User=require("../models/User")
+
 exports.login=function(req,res){
     
 }
@@ -7,7 +9,10 @@ exports.logout=function(req,res){
 }
 
 exports.register=function(req,res){
-    res.send("sss")
+    //new object with constructor of User in model/User.
+    let user=new User(req.body)
+    user.register()
+    res.send("todo")
 }
 
 exports.home=function(req,res){
