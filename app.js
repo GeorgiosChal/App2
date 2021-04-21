ed82b5d915fb2ce403be10d85a806a7d9037c7f5
GeorgeChal <daxter132@gmail.com>
2021-04-21 18:54:32 +0300

    Start of MVC

---
+++ app.js
@@ -0,0 +1,20 @@
+const express =require('express')
+const app=express()
+
+//sets a router. value of router should be whatever returned
+//from router.js file with line:module.exports=router
+const router=require('./router')
+
+//enable public folder to be available on client side
+app.use(express.static('public'))
+//we are setting view templates with the name views <<2nd arg is the name>>
+app.set('views','views')
+//for views, we set the engine as ejs
+app.set('view engine','ejs')
+
+
+//Requests
+app.use('/',router)
+
+
+app.listen(3000) 
---
+++ router.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const router=express.Router()
+//we "imclude here the following file as userController"
+//router should only do routing tasks, the functions is included 
+//in require variables as part of the MVC (Model,View,Controller)
+const userController=require('./controllers/userController')
+
+router.get('/',userController.home)
+router.post('/register',userController.register)
+
+//router will return on where is been required. in this case app.js
+module.exports=router
